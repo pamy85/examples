3eebbcf495acb3fe1ee7356d70ffc683a865dfd4
Jesus Pau <jpau@paradigmadigital.com>
2018-10-14 09:46:22 +0200

    uploading springboot-url basic example

---
+++ springboot-mvc-urls/src/main/java/wecc/examples/springboot/mvc/urls/ExampleMvcUrlsApplication.java
@@ -0,0 +1,13 @@
+package wecc.examples.springboot.mvc.urls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleMvcUrlsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleMvcUrlsApplication.class, args);
+	}
+}
+
+
---
+++ springboot-mvc-urls/src/main/java/wecc/examples/springboot/mvc/urls/controllers/ExampleController.java
@@ -0,0 +1,21 @@
+package wecc.examples.springboot.mvc.urls.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ExampleController {
+
+    @GetMapping("/view")
+    public String query(@RequestParam String view) {
+        return view;
+    }
+
+    @GetMapping("/view/{view}")
+    public String path(@PathVariable String view) {
+        return view;
+    }
+
+}
