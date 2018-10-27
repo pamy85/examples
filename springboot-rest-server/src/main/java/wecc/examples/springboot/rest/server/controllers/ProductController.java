852bb53378fb814539de9eef693fbf4cbeead89c
Jesus Pau <jpau@paradigmadigital.com>
2018-10-27 13:33:27 +0200

    adding a ResponseEntity example

--- springboot-rest-server/src/main/java/wecc/examples/springboot/rest/server/controllers/ProductController.java
+++ springboot-rest-server/src/main/java/wecc/examples/springboot/rest/server/controllers/ProductController.java
@@ -1,12 +1,15 @@
 package wecc.examples.springboot.rest.server.controllers;
 
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import wecc.examples.springboot.rest.server.model.Product;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestController
@@ -18,6 +21,16 @@ public class ProductController {
     @ResponseStatus(HttpStatus.OK)
     public List<Product> products(){
         return products.values().stream().collect(Collectors.toList());
+    }
+
+    @GetMapping("/product/{code}")
+    public ResponseEntity<Product> products(@PathVariable String code){
+        Product product = products.get(code);
+        if (Objects.isNull(product)){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else{
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }
     }
 
     @PostMapping("/product")
