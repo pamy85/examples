c5bdaf8f03c353b97f434e9c6115fa94bdaf05f5
Jesus Pau <jpau@paradigmadigital.com>
2018-11-02 17:01:39 +0100

    remove unused imports

--- springboot-test-cucumber/src/test/java/wecc/examples/springboot/test/cucumber/AddProduct.java
+++ springboot-test-cucumber/src/test/java/wecc/examples/springboot/test/cucumber/AddProduct.java
@@ -1,6 +1,5 @@
 package wecc.examples.springboot.test.cucumber;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
